f7e24e4ea90cb861fa8cf08b915a07479a7c4088
ciuz99best <manolachegabriel637@gmail.com>
2020-06-09 00:46:17 +0300

    made the pages responsive

--- includes/Views/Contact.php
+++ includes/Views/Contact.php
@@ -61,64 +61,64 @@ else
 <body>
 
 <header>
-    <div class="header-area">
-        <div class="header-logo">
-            <picture>
-                <img src="Images/Home/OECD.png" title="OECD">
-            </picture>
-        </div>
-        <div class="header-social">
-            <ul id="socials">
-                <li>
-                    <a href="#" title="Facebook">
-                        <i class="fa fa-facebook">
-                        </i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" title="Pinterest">
-                        <i class="fa fa-pinterest">
-                        </i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" title="LinkedIn">
-                        <i class="fa fa-linkedin">
-                        </i>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" title="Twitter">
-                        <i class="fa fa-twitter">
-                        </i>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <div class="top_banner">
-        <picture>
-            <img src="Images/Home/banner_top.png" title="PISA">
-        </picture>
-    </div>
-    <nav>
-        <div class="nav-sticky">
-            <div class="nav-menu">
-                <ul id="navigation">
-                    <a href="index.php"><li>
-                        Home
-                    </li></a>
-                    <a href="Results.php"><li>
-                        Results
-                    </li></a>
-                    <a href="Contact.php"><li>
-                        Contact
-                    </li></a>
-                </ul>
+            <div class="header-area">
+                <div class="header-logo">
+                    <picture>
+                        <img src="Images/Home/OECD.png" title="OECD">
+                    </picture>
+                </div>
+                <div class="header-social">
+                    <ul id="socials">
+                        <li>
+                            <a href="https://www.facebook.com/OECDEduSkills" title="Facebook">
+                                <i class="fa fa-facebook">
+                                </i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/user/EDUContact" title="Youtube">
+                                <i class="fa fa-youtube">
+                                </i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://oecdedutoday.com/" title="Blogs">
+                                <i class="fa fa-envelope">
+                                </i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/hashtag/oecdpisa" title="Twitter">
+                                <i class="fa fa-twitter">
+                                </i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-</header>
+            <div class="top_banner">
+                <picture>
+                    <img src="Images/Home/banner_top.png" title="PISA">
+                </picture>
+            </div>
+            <nav>
+                <div class="nav-sticky">
+                    <div class="nav-menu">
+                        <ul id="navigation">
+                            <a href="Index.php"><li>
+                                Home
+                            </li></a>
+                            <a href="Results.php"><li>
+                                Results
+                            </li></a>
+                            <a href="Contact.php"><li>
+                                Contact
+                            </li></a>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
     <main>
         <div class="container">
             <div class="head" >
