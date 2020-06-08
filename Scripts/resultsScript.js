9edc1da05324af81d52079267ba076bd24361f31
ciuz99best <manolachegabriel637@gmail.com>
2020-06-08 08:17:20 +0300

    Added export as png,svg and made home more responsive

--- Scripts/resultsScript.js
+++ Scripts/resultsScript.js
@@ -1,5 +1,6 @@
 let chartType = 'bar';
 
+var data=null;
 window.onload = function ()
 {
     document.getElementById("testButton").addEventListener("click", function(){
@@ -10,6 +11,9 @@ window.onload = function ()
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("myChart").remove(); 
             div = document.querySelector("#chartContainer");
+            document.getElementById("exportPNG").style.display="inline";
+            document.getElementById("exportCSV").style.display="inline";
+            document.getElementById("exportSVG").style.display="inline";
             div.insertAdjacentHTML("afterbegin", "<canvas id='myChart'></canvas>");
             let scores = JSON.parse(this.responseText);
             let countries = [];
@@ -21,7 +25,7 @@ window.onload = function ()
             }
 
             const ctx = document.getElementById("myChart").getContext('2d');
-            const chart = new Chart(ctx, {
+            const graphData={
                 type: 'bar',
                 data: {
                     labels: countries,
@@ -56,11 +60,54 @@ window.onload = function ()
                         }]
                     }
                 }
-            });
-
+            }
+            console.log(means);
+            graphData.options.responsive=false;
+            data=graphData;
+            const chart = new Chart(ctx, graphData);
         }
         };
         xmlhttp.open("GET", "http://127.0.0.1/PIRO-PISA-ROMANIA/Results.php?c1=".concat(c1).concat("&c2=").concat(c2), true);
         xmlhttp.send();
     });
+}
+function download(type) {
+    var graphData=data;
+    graphData.options.animation=false;
+    graphData.options.responsive=false;
+
+    const ctx = document.getElementById("myChart");
+    var width=ctx.getBoundingClientRect().width;
+    var height=ctx.getBoundingClientRect().height;
+    const svgContext = C2S(width,height);
+    const mySvg = new Chart(svgContext, graphData);
+    mySvg.width=width;
+    mySvg.height=height;
+
+    var dl = document.createElement("a");
+    document.body.appendChild(dl); // This line makes it work in Firefox.
+
+    if (type=="SVG") {
+        var svg = svgContext.getSvg();
+        if (window.ActiveXObject) {
+            svgString = svg.xml;
+        } else {
+            var oSerializer = new XMLSerializer();
+            svgString = oSerializer.serializeToString(svg);
+        }
+        dl.download = "Chart.svg";
+        //   return "data:image/svg+xml," + encodeURIComponent(svgAsXML);
+        dl.href = 'data:image/svg+xml;utf8,' + encodeURIComponent(svgString);
+    }
+    if(type=="PNG") {
+        var canvas=document.getElementById("myChart");
+        var img=canvas.toDataURL("image/png");
+        dl.download="Chart.png";
+        dl.href=img;
+    }
+    if(type="CSV") {
+
+    }
+    dl.click();
+    document.body.removeChild(dl);
 }
--- includes/Views/Results.php
+++ includes/Views/Results.php
@@ -2,9 +2,10 @@
 <html lang="en-US">
 <head>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.bundle.min.js"></script>
+    <script type="text/javascript" src="Scripts/canvas2svg.js"></script>
     <script src="Scripts/resultsScript.js"></script>
     <link rel="stylesheet" type="text/css" href="StyleSheets/style.css">
-    <link rel="stylesheet" type="text/css" href="StyleSheets/StyleHome.css">
+    <link rel="stylesheet" type="text/css" href="StyleSheets/StyleResults.css">
     <link rel="icon" href="Images/Home/oecd_logo.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PiSa Statistics</title>
@@ -70,6 +71,7 @@
                 </div>
             </nav>
         </header>
+        <main>
         <form id="inputForm">
             <input type="text" id="c1" name="c1">
             <input type="text" id="c2" name="c2">
@@ -77,10 +79,13 @@
         </form>
         <p id="temp"></p>
         <button id="testButton"> Get Chart </button>
-        
+        <button id="exportPNG" style="display:none;" onclick="download('PNG')"> Export PNG</button>
+        <button id="exportSVG" style="display:none;" onclick="download('SVG')"> Export SVG</button>
+        <button id="exportCSV" style="display:none;" onclick="download('CSV')"> Export CSV</button>
         <div id="chartContainer" height="400px" width = "400px">
             <canvas id="myChart"></canvas>
         </div>
+        </main>
     </body>
     
 </html>
