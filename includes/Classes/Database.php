66f7bdf00a25467564143fdc20d5dcc07a22af0b
ciuz99best <manolachegabriel637@gmail.com>
2021-04-20 23:18:25 +0300

    asd

--- includes/Classes/Database.php
+++ includes/Classes/Database.php
@@ -10,7 +10,7 @@ class Database
     public static function getConnection(){
         if (is_null(self::$connection))
         {
-            self::$connection = new PDO('mysql:host=localhost;dbname=pisa statistics', 'root', '');
+            self::$connection = new PDO('mysql:host=myserver12.mysql.database.azure.com;dbname=pisa statistics', 'ciuz99best@myserver12', 'Parola123!');
             self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$connection;
